5bd649195b9405ec00b9466df5a962027197838a
Giovanni Euresti <gioeuresti123@gmail.com>
2020-06-03 12:35:19 -0500

    Added view which displays post titles

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def generic(request):
+    return render(request, 'blog/generic.html', {})
+
+def post_list(request):
+    args = Post.objects.all()
+    titles = {'titles':args}
+    return render(request, 'blog/post_list_template.html', titles)
