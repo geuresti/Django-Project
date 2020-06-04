d856f916e9dd3947ffddf70057c4b2cf83ea1a66
Giovanni Euresti <gioeuresti123@gmail.com>
2020-06-04 11:15:55 -0500

    added better template and /generic url, homepage should display all articles

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_titles'),
-    
+    path('generic', views.generic, name='generic'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+#    v-- current directory
 from .models import Post
 
 def generic(request):
     return render(request, 'blog/generic.html', {})
 
 def post_list(request):
-    args = Post.objects.all()
-    titles = {'titles':args}
-    return render(request, 'blog/post_list_template.html', titles)
+    #args = Post.objects.all()
+    #titles = {'title':args}
+    #return render(request, 'blog/post_list_template.html', titles)
+    posts = Post.objects.order_by('created_date')
+    #<QuerySet [<Post: Test Post>, <Post: WA#8 Rough draft>, <Post: poster>, <Post: Bard>, <Post: Blogger>, <Post: Sample Text>, <Post: ABC>]>
+    return render(request, 'blog/post_list_template.html', {'posts':posts})
