fff7463040e3c72c0527b27cb34149d33d1b2765
Giovanni Euresti <gioeuresti123@gmail.com>
2020-06-07 15:04:17 -0500

    added post details and worked on the templates, added CSS and used Bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_titles'),
     path('generic', views.generic, name='generic'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 #    v-- current directory
 from .models import Post
 
@@ -12,3 +12,7 @@ def post_list(request):
     posts = Post.objects.order_by('created_date')
     #<QuerySet [<Post: Test Post>, <Post: WA#8 Rough draft>, <Post: poster>, <Post: Bard>, <Post: Blogger>, <Post: Sample Text>, <Post: ABC>]>
     return render(request, 'blog/post_list_template.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
